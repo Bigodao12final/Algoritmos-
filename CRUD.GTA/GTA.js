84716600eb33066f87b89d76803114f3611ea92d
Bigodao12final <renanredhotchillipeppers@gmail.com>
2024-11-28 12:43:43 -0300

    Update GTA.js

--- CRUD.GTA/GTA.js
+++ CRUD.GTA/GTA.js
@@ -1,1 +1,13 @@
+class Gta {
+    constructor(Nome, AnoQSP, dataLancamento, Protagonista, Antagonista, Cidade,  posicaoNaLista) {
+        this.Nome = Nome;
+        this.AnoQSP = AnoQSP;
+        this.dataLancamento = dataLancamento;
+        this.Protagonista= Protagonista;
+        this.Antagonista = Antagonista;
+        this.Cidade= Cidade;
 
+
+        this.posicaoNaLista = posicaoNaLista; //atributo para facilitar a alteração e exclusão 
+    }
+}
